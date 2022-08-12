b17237d128ee57006da97143ebf13b8fc46fb64f
Elijah Woodhouse <elijahwoodhouse95@gmail.com>
2022-08-12 17:31:09 -0600

    got the coordinates in getXY and returned a message with those coordinates in getXYMessage

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -6,7 +6,7 @@ const initialMessage = ''
 const initialEmail = ''
 const initialSteps = 0
 const initialIndex = 4
-const board = ["", "", "", "", "B", "", "", "", ""]
+const board = ["", "", "", "", "", "", "", "", ""]
  // the index the "B" is at
 
 const initialState = {
@@ -44,25 +44,42 @@ export default class AppClass extends React.Component {
       board: newBoard,
       currentIndex: this.move(this.state.currentIndex),
       steps: this.state.steps + 1,
-      movesX: this.state.currentIndex === "B" ? this.state.movesX + 1 : this.state.movesX,
-      movesY: this.state.currentIndex === "B" ? this.state.movesY + 1 : this.state.movesY,
     })
   }
 
   getXY = (index) => {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
-    this.state.board.map((square, idx) => {
-      if (idx === index){
-
-      }
-    })
+    var coordinates = []
+    if (index === 0) {
+      coordinates = [ 1, 1 ]
+    } else if(index === 1) {
+      coordinates = [ 1, 2 ]
+    } else if(index === 2) {
+      coordinates = [ 1, 3 ]
+    } else if(index === 3) {
+      coordinates = [ 2, 1 ]
+    } else if(index === 4) {
+      coordinates = [ 2, 2 ]
+    } else if(index === 5) {
+      coordinates = [ 2, 3 ]
+    } else if(index === 6) {
+      coordinates = [ 3, 1 ]
+    } else if(index === 7) {
+      coordinates = [ 3, 2 ]
+    } else if(index === 8) {
+      coordinates = [ 3, 3 ]
+    }
+    console.log(coordinates)
+    return coordinates
   }
 
-  getXYMessage = () => {
+  getXYMessage = (index) => {
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
+    const cords = getXY(index)
+    return (`${cords[0]}, ${cords[1]}`)
   }
 
   reset = () => {
@@ -96,17 +113,17 @@ export default class AppClass extends React.Component {
 
   render() {
     const { className } = this.props
-    const { board, currentTurn, coordinates } = this.state
+    const { board, currentTurn, coordinates, steps } = this.state
     return (
       <div id="wrapper" className={className}>
         <div className="info">
           <h3 id="coordinates">{coordinates}</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="steps">You moved {steps} times</h3>
         </div>
         <div id="grid">
           {
             board.map((val, idx) => {
-              return (<div key={idx} onClick={() => this.handleTurn(idx)} className={`square${val ? ' active' : ''}`}>
+              return (<div key={idx} onClick={() => this.getXYMessage(idx)} className={`square${val ? ' active' : ''}`}>
                 {val}
               </div>
             );
